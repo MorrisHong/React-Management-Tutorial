6ed10dbe65e05e4aa125b2b1cfb4c3b23064cbdf
gracelove <govlmo91@gmail.com>
2019-05-14 18:46:07 +0900

    5/14

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Customer from './components/Customer';
 
 const customer ={
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
   'name':'홍은모',
   'birthday':'910628',
   'gender':'남자',
@@ -11,6 +13,8 @@ const customer ={
 function App() {
   return (
     <Customer
+      id={customer.id}
+      image = {customer.image}
       name={customer.name}
       birthday={customer.birthday}
       gender={customer.gender}
